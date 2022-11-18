46a4c92483c51069859c96c4b89850ded1723fbb
shahman-riaz <shahmanriaz07@gmail.com>
2022-11-18 19:02:23 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import {Navbar, Hero, Stats, Billing, Footer, FeedbackCard, Business, Testimonials, CardDeal, CTA, Clients} from "./components";
+
+import styles from "./style";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className='bg-primary w-full overflow-hidden'>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
+
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats /> 
+          <Business />
+          <Billing />
+          <CardDeal /> 
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
+        </div>
+      </div>
+
+
+
+   </div>
   );
 }
 
---
+++ src/components/index.js
@@ -0,0 +1,18 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import Business from "./Business";
+import Button from "./Button";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import FeedbackCard from "./FeedbackCard";
+import Footer from "./Footer";
+import GetStarted from "./GetStarted";
+import Stats from "./Stats";
+import Hero from "./Hero";
+import Testimonials from "./Testimonials";
+
+
+export  {
+    Navbar, Billing, CardDeal, Business, Button, Clients, CTA, FeedbackCard, Footer, GetStarted, Stats, Hero, Testimonials
+}
